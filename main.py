93b64115cdcd8bd2af7c4fb57870be9d5cb9a784
Nathan Gilman <ngilman@ufl.edu>
2024-06-09 12:02:05 -0400

    Rework recursive parsing.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 import os
+import re
 
 
 def read_yml(filepath):
@@ -10,34 +11,55 @@ def read_yml(filepath):
     return data
 
 
-def parse_requirements(role_data):
-    return []
+def parse_yml_section(section):
+    role_requirements = []
+    role_variables = []
+    role_dependencies = []
+
+    for (key, value) in section.items():
+        if isinstance(value, dict):
+            result = parse_yml_section(value)
+            for req in result[0]:
+                role_requirements.append(req)
+            for var in result[1]:
+                role_variables.append(var)
+            for dep in result[2]:
+                role_dependencies.append(dep)
+        elif isinstance(value, list):
+            pass
+        else:
+            # need to add requirements
+
+            # search for variables, currently handles only non default variable
+            match_var = re.search(r'{{ [a-zA-Z_-]* }}', str(value))
+            if (match_var):
+                # print(match_var)
+                role_variables.append(match_var.group(0))
+            # search for dependencies, currently handles only roles with FQCN
+            match_dep = re.search(r'[a-zA-Z-]*\.[a-zA-Z-]*\.[a-zA-Z-]*', key)
+            if (match_dep):
+                role_dependencies.append(match_dep.group(0))
+
+    return (role_requirements,
+            role_variables,
+            role_dependencies)
 
 
-def parse_variables(role_data):
-    for key, value in role_data.items():
-        if ("{{" in value):
-            pass  # process line
-    return []
-
-
-def parse_dependencies(role_data):
-    return []
-
-
-def parse_data(role_data, fqcn):
+# var name|default value (t/f)|required (t/f)|description|example|
+def parse_yml(role_data, fqcn):
     role_name = fqcn
     role_description = f"< enter {fqcn} description >"
-
-    # requirements
-    role_requirements = parse_requirements(role_data)
-
-    # variables
-    # var name | default value (t/f) | required (t/f) | description | example |
-    role_variables = parse_variables(role_data)
-
-    # dependencies
-    role_dependencies = parse_dependencies(role_data)
+    role_requirements = []
+    role_variables = []
+    role_dependencies = []
+    for section in role_data:
+        result = parse_yml_section(section)
+        for req in result[0]:
+            role_requirements.append(req)
+        for var in result[1]:
+            role_variables.append(var)
+        for dep in result[2]:
+            role_dependencies.append(dep)
 
     # misc
     role_example_playbook = ""
@@ -54,29 +76,26 @@ def parse_data(role_data, fqcn):
             role_author_info)
 
 
-namespace = "redhat"
-collection = "controller_configuration"
-container = {
-    namespace: {
-        collection: {}
+def build_container(namespace, collection):
+    container = {
+        namespace: {
+            collection: {}
+        }
     }
-}
-
-
-def build_container():
     roles_path = f"../{collection}/roles"
 
     for role in os.listdir(roles_path):
         role_path = f"{roles_path}/{role}"
         main_path = f"{role_path}/tasks/main.yml"
         if os.path.isdir(role_path) and os.path.isfile(main_path):
-            container[namespace][collection][role] = read_yml(main_path)
+            container[namespace][collection][role] = parse_yml(read_yml(main_path), f"{collection}.{role}")
+    return container
 
 
-# build_container()
-# print(json.dumps(container, indent=4))
+container = build_container("redhat", "controller_configuration")
+print(json.dumps(container, indent=4))
 
 file = "../controller_configuration/roles/bulk_host_create/tasks/main.yml"
 
-print(json.dumps(read_yml(file), indent=4))
-print(parse_data(read_yml(file), "controller_configuration.bulk_host_create"))
+# print(json.dumps(read_yml(file), indent=4))
+# print(parse_yml(read_yml(file), "controller_configuration.bulk_host_create"))
